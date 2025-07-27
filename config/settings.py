8a6907d67e858e49d1a210e1dd19f9e242ccf1c9
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-07-27 19:36:04 -0400

    Fix: workflow cleanup and optimized

--- config/settings.py
+++ config/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
