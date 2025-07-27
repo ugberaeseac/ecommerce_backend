f2ead3d76ff7d6b038c3749e8a76c13bff0ee5e1
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-07-27 12:57:25 -0400

    feat: setup Django GitHub Actions workflow

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-09-!fs(91%u8vy+dsw&5^573dyeft-r#$)2n@oi9b*c*+m-(+b'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-09-!fs(91%u8vy+dsw&5^573dyeft-r#$)2n@oi9b*c*+m-(+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
