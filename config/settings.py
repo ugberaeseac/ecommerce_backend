19540e40295db62a302b7d6c59d6de91f24aecec
UGBERAESE Charles A. <120336488+ugberaeseac@users.noreply.github.com>
2025-08-28 17:35:10 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-#SECRET_KEY = 'django-insecure-09-!fs(91%u8vy+dsw&5^573dyeft-r#$)2n@oi9b*c*+m-(+b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
